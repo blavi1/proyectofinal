28ec496c7709a5a87b7fc5abf4728f7b389c92bb
Bartolome <bartolome@intelligenia33.intelligenia.lan>
2017-05-17 12:39:18 +0200

    C-17-05-17-M

--- frontend/js/configs.js
+++ frontend/js/configs.js
@@ -22,30 +22,7 @@ jQuery(document).ready(function ($) {
             $desplegable.toggleClass("desplegado");
         }
 	});
-	/*$(".desplegableAbstract, .desplegador").click(function(e) {
-            e.preventDefault();
-            var $desplegable = $(this).parent().find('.desplegableAbstract');
-            if ($(e.target).hasClass("closeX")) {
-                return;
-            }
-            if ($desplegable.hasClass("desplegado") || $(e.target).hasClass('globalCloseX')) {
-                $(".desplegableAbstract").removeClass("desplegado");
-            } else {
-                $(".desplegableAbstract").removeClass("desplegado");
-                $desplegable.toggleClass("desplegado");
-            }
-        });
-    $(document).mouseup(function(e) {
-            var $desplegable = $(e.target).closest('.desplegableAbstract');
-            if ($desplegable.length > 0 && !$(e.target).hasClass('globalCloseX')) {
-                return false;
-            }
-            if (!$(e.target).hasClass('desplegador')) {
-                $(".desplegado").removeClass("desplegado");
-            }
-            return true;
-    });
-*/
+	
     /*$(document).ready(function (e) {
 	    $(".poyua").click(function(e){
 	        alert("Hola" + e.target.nodeName);
